c379650b6250f1b12afee759cfe1e0996e5c6143
Hector <hectoremil21@hotmail.com>
2021-11-14 19:49:42 -0500

    Initialize WinSock in client and server.

--- bbKvm-vc/Server.cpp
+++ bbKvm-vc/Server.cpp
@@ -1,9 +1,17 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <stdio.h>
+#include <iostream>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 void server_main() {
+	WSADATA wsaData;
+	int wsOk = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsOk != 0)
+	{
+		std::cout << "WSAStartup failed: " << wsOk << std::endl;
+	}
 
+	WSACleanup();
 }
