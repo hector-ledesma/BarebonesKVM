d5490617ffd5f0a6684b128d9ddc106598455818
Hector <hectoremil21@hotmail.com>
2021-12-09 22:29:37 -0500

    getaddrinfo() uses wildcard for server and address for client.

--- bbKvm-vc/TCPSocketController.cpp
+++ bbKvm-vc/TCPSocketController.cpp
@@ -62,46 +62,51 @@ TCPSocketController::generateSocket(bool isServer) {
 	hints.ai_socktype = SOCK_STREAM;	//	Stream Socket
 	hints.ai_protocol = IPPROTO_TCP;	//	TCP Protocol
 	int wsOk;
+	char ipstr[INET6_ADDRSTRLEN];
 	if (isServer)
+	{
 		hints.ai_flags = AI_PASSIVE;	//	Caller will use returned socket in bind() call. Server listening socket.
+		wsOk = getaddrinfo(nullptr, m_port, &hints, &m_result);
+	}
+	else
+		wsOk = getaddrinfo(m_address, m_port, &hints, &m_result);
 
-	wsOk = getaddrinfo(m_address, m_port, &hints, &m_result);
 	if (wsOk != 0)
 	{
 		std::cerr << "\n[TCPSocketController] ---- geraddrinfo failed: " << wsOk << std::endl;
 		throw wsOk;
 	}
-	else
-	{
-		struct sockaddr_in* ipv4 = (struct sockaddr_in*)m_result->ai_addr;
-		char ipstr[INET6_ADDRSTRLEN];
-		inet_ntop(m_result->ai_family, &(ipv4->sin_addr), ipstr, sizeof ipstr);
-		std::cout << "\n[TCPSocketController] ---- getaddrinfo() resolved successfully:\n"
-			<< "\t|---- Address: \t" << ipstr << "\n"
-			<< "\t|---- Family: \t" << m_result->ai_family << "\n"
-			<< "\t|---- SckType: \t" << m_result->ai_socktype << "\n"
-			<< "\t|---- Protocol: \t" << m_result->ai_protocol << "\n";
-	}
+
+	struct sockaddr_in* ipv4 = (struct sockaddr_in*)m_result->ai_addr;
+	//char ipstr[INET6_ADDRSTRLEN];
+	inet_ntop(m_result->ai_family, &(ipv4->sin_addr), ipstr, sizeof ipstr);
+	std::cout << "\n[TCPSocketController] ---- getaddrinfo() resolved successfully:\n"
+		<< "\t|---- Address: \t" << ipstr << "\n"
+		<< "\t|---- Family: \t" << m_result->ai_family << "\n"
+		<< "\t|---- SckType: \t" << m_result->ai_socktype << "\n"
+		<< "\t|---- Protocol: \t" << m_result->ai_protocol << "\n";
+	
 
 	// Testing
+	std::cout << "\n[TCPSocketController] ---- Testing all addresses:\n" << std::endl;
 	for (addrinfo* p = m_result; p != nullptr; p = p->ai_next)
 	{
 		struct sockaddr_in* ipv4 = (struct sockaddr_in*)m_result->ai_addr;
 		char ipstr[INET6_ADDRSTRLEN];
 		inet_ntop(m_result->ai_family, &(ipv4->sin_addr), ipstr, sizeof ipstr);
-		std::cout << "\n[TCPSocketController] ---- Testing all addresses:\n"
-			<< "\t|----  \t" << ipstr << std::endl;
+		std::cout << "\t|----  \t" << ipstr << std::endl;
 	}
 
 	workSocket = socket(m_result->ai_family, m_result->ai_socktype, m_result->ai_protocol);
 	if (workSocket == INVALID_SOCKET)
 	{
-		std::cerr << "\n[TCPSocketController] ---- Error generating socket: " << WSAGetLastError() << std::endl;
-		throw - 1;
+		int err = WSAGetLastError();
+		std::cerr << "\n[TCPSocketController] ---- Error generating socket: " << err << std::endl;
+		throw err;
 	}
 	
 	std::cout << "\n[TCPSocketController] ---- Successfully created Socket Data:" << std::endl;
-	std::cout << "\t |---- Socket address:" << workSocket << std::endl;
+	std::cout << "\t |---- Socket address:" << ipstr << std::endl;
 
 	return workSocket;
 }
